c17beed7053858eca3942a18d3b91b327468e98a
Yulia <linguavoice1@yahoo.com>
2024-12-16 14:26:24 +0300

    fix: all algorithms solved

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+  class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
 class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+     return this.head;
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+     const newNode  = new Node(value);
+     if (this.tail) {
+        this.tail.next = newNode;
+     }
+     this.tail = newNode;
+     if (!this.head) {
+       this.head = newNode;
+     }
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return undefined;
+    }
+    const value  = this.head.value;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = null;
+    }
+    return value;
   }
 }
 
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+   const dummy  = { next: l };
+   let current = dummy;
+   let nextNode = l;
+
+   while (nextNode !== null) {
+    if (nextNode.value === k) {
+      current.next = nextNode.next;
+    } else {
+      current = nextNode;
+    }
+    nextNode = nextNode.next;
+   }
+   return dummy.next;
 }
 
 module.exports = {
